4ded67a3358f9614dd618139d378446dc3bd526d
billyhq <billyhq@gmail.com>
2014-02-11 23:46:59 +0800

    this is for git test

--- awk_test.sh
+++ awk_test.sh
@@ -3,5 +3,5 @@
 line=$(last -F Billy | grep '.*Dec.*2013' | grep -v 'crash' | grep -v '+' | wc -l)
 echo $line
 last -F Billy | grep '.*Dec.*2013' | grep -v 'crash' | grep -v '+' | sed 's/(//g' | sed 's/)//g' | awk '{print $15}' | awk 'BEGIN{FS=":"} NR==1{hh=0;mm=0;} {hh=hh+$1;mm=mm+$2} NR==4{print hh "\t" mm}'
-
+#for test
 
